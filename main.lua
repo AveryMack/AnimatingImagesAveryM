9808c8c7291fcb945eb4d075aca64e27403d266b
Avery Mack <avery.mack@ocsbstudent.ca>
2018-03-06 10:44:31 -0500

    vh

--- main.lua
+++ main.lua
@@ -1,7 +1,27 @@
 -----------------------------------------------------------------------------------------
---
+-- Title: Animating Images
+-- Name: Avery Mack
+-- Course: ICS2O/3C
+-- This program displays 3 images moving in different directions, 
+-- with some colour text and a background 
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
